aaff99e6ee6fb4627fcf07780ef9e5e858e6a04c
Nick Newman <nncsec@gmail.com>
2019-08-27 15:05:48 -0500

    slider animation

--- web/main.dart
+++ web/main.dart
@@ -18,7 +18,8 @@ Future<Null> main() async {
 
   var resourceManager = ResourceManager();
   resourceManager.addBitmapData("player0", "images/player0.png");
-  resourceManager.addBitmapData("player1", "images/player1.png");
+  resourceManager.addBitmapData("sliderBackground", "images/sliderBackground.png");
+  resourceManager.addBitmapData("slider0", "images/slider0.png");
 
   var keyboard = new Keyboard();
 
@@ -28,17 +29,48 @@ Future<Null> main() async {
   var player = Sprite();
   player.addChild(Bitmap(playerData));
 
+  var sliderBackgroundData = resourceManager.getBitmapData("sliderBackground");
+  var sliderBackground = Sprite();
+  sliderBackground.addChild(Bitmap(sliderBackgroundData));
+
+  var sliderData = resourceManager.getBitmapData("slider0");
+  var slider = Sprite();
+  slider.addChild(Bitmap(sliderData));
+
   player.pivotX = playerData.width / 2;
   player.pivotY = playerData.height / 2;
 
-  player.x = 800 / 2;
-  player.y = 800 / 2;
+  player.x = 100;
+  player.y = 100;
 
   stage.addChild(player);
 
-  stage.onKeyDown.listen((e) {
-      var playerUp = stage.juggler.addTween(player, 0.1, Transition.linear);
-      playerUp.animate.y.to(player.y-5);
+  sliderBackground.pivotX = sliderBackground.width / 2;
+  sliderBackground.pivotY = sliderBackground.width / 2;
+
+  sliderBackground.x = 400;
+  sliderBackground.y = 600;
+
+  stage.addChild(sliderBackground);
+
+  slider.pivotX = slider.width / 2;
+  slider.pivotY = slider.height / 2;
+
+  slider.x = 216;
+  slider.y = 425;
+
+  stage.addChild(slider);
+
+  stage.onMouseClick.listen((e) {
+    var sliderToRight =
+    stage.juggler.addTween(slider, 1.5, Transition.linear);
+    sliderToRight.animate.x.to(slider.x+368);
+  });
+
+  stage.onMouseRightClick.listen((e) {
+    var sliderStop;
+    stage.juggler.addTween(slider, 0.1, Transition.linear);
+    sliderStop.animate.x.to(slider.x);
   });
 
 /*
