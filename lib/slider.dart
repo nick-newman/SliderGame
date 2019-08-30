127580699303d889a2973227a347897210aa2dde
nick-newman <44561033+nick-newman@users.noreply.github.com>
2019-08-30 00:57:14 -0500

    updated slider

--- lib/slider.dart
+++ lib/slider.dart
@@ -6,146 +6,134 @@ import 'dart:math';
 
 class Slider extends DisplayObjectContainer {
 
-var background;
-var foreground;
-var indicator;
-var indicatorBg;
-var indicatorFg;
+var sliderBackground;
+var sliderForeground;
 var target;
+var indicatorBackground;
+var indicatorForeground;
+bool looping = false;
+Tween moveIndicatorBackground, moveIndicatorForeground;
 
 Slider() {
 
-  this.background = new Sprite();
-  this.foreground = new Sprite();
-  this.indicator = new Sprite();
-  this.indicatorBg = new Sprite();
-  this.indicatorFg = new Sprite();
-  this.target = new Sprite();
+  sliderBackground = Bitmap(BitmapData(400, 50, Color.White, 4));
+  sliderBackground.pivotX = sliderBackground.width / 2;
+  sliderBackground.pivotY = sliderBackground.height / 2;
+  sliderBackground.x = 200;
+  sliderBackground.y = 200;
+  addChild(sliderBackground);
 
-  var background = BitmapData(400, 50, Color.White);
-  var backgroundBitmap = Bitmap(background);
-  addChild(backgroundBitmap);
+  sliderForeground = Bitmap(BitmapData(392, 42, Color.Black));
+  sliderForeground.pivotX = sliderForeground.width / 2;
+  sliderForeground.pivotY = sliderForeground.height / 2;
+  sliderForeground.x = sliderBackground.x;
+  sliderForeground.y = sliderBackground.y;
+  addChild(sliderForeground);
 
-  var foreground = BitmapData(390, 42, Color.Black);
-  var foregroundBitmap = Bitmap(foreground);
-  addChild(foregroundBitmap);
+  generateTarget();
 
-  var indicatorBg = BitmapData(14, 42, Color.Black);
-  var indicatorBgBitmap = Bitmap(indicatorBg);
-  addChild(indicatorBgBitmap);
+  generateIndicator();
 
-  var indicatorFg = BitmapData(5, 34, Color.White);
-  var indicatorFgBitmap = Bitmap(indicatorFg);
-  addChild(indicatorFgBitmap);
-
-  //30 should be random
-  var target = BitmapData(30, 34, Color.Green);
-  var targetBitmap = Bitmap(target);
-  addChild(targetBitmap);
-
-  backgroundBitmap.pivotX = backgroundBitmap.width / 2;
-  backgroundBitmap.pivotY = backgroundBitmap.height / 2;
-  backgroundBitmap.x = 100;
-  backgroundBitmap.y = 100;
-
-  foregroundBitmap.pivotX = foregroundBitmap.width / 2;
-  foregroundBitmap.pivotY = foregroundBitmap.height / 2;
-  foregroundBitmap.x = backgroundBitmap.x;
-  foregroundBitmap.y = backgroundBitmap.y;
-
-  targetBitmap.pivotX = targetBitmap.width / 2;
-  targetBitmap.pivotY = targetBitmap.height / 2;
-  targetBitmap.x = backgroundBitmap.x;
-  //y should be random inside slider
-  targetBitmap.y = backgroundBitmap.y;
-
-  indicatorBgBitmap.pivotX = indicatorBgBitmap.width / 2;
-  indicatorBgBitmap.pivotY = indicatorBgBitmap.height / 2;
-  indicatorBgBitmap.x = backgroundBitmap.x - 187;
-  indicatorBgBitmap.y = backgroundBitmap.y;
-
-  indicatorFgBitmap.pivotX = indicatorBgBitmap.width / 2;
-  indicatorFgBitmap.pivotY = indicatorFgBitmap.height / 2;
-  indicatorFgBitmap.x = indicatorBgBitmap.x + 4;
-  indicatorFgBitmap.y = indicatorBgBitmap.y;
-
-  Tween indicatorBgSlide, indicatorFgSlide;
+  //On mouse click or press v key, start indicator
+  html.window.onMouseDown.listen((e) {
+    //Start moving the indicator
+    moveIndicatorBackground = stage.juggler.addTween(indicatorBackground, 1.6, Transition.linear);
+    moveIndicatorBackground.animate.x.to(indicatorBackground.x + 384);
+    moveIndicatorForeground = stage.juggler.addTween(indicatorForeground, 1.6, Transition.linear);
+    moveIndicatorForeground.animate.x.to(indicatorForeground.x + 384);
+    //moveIndicatorBackground.onComplete = () => moveIndicatorBackground = null;
+    //moveIndicatorForeground.onComplete = () => moveIndicatorForeground = null;
+    indicator();
+    looping = true;
+  });
 
   html.window.onKeyPress.listen((e) {
-
-    if (e.key == 'c') {
-      if (indicatorBgSlide != null) return;
-      if (indicatorFgSlide != null) return;
-      indicatorBgSlide = stage.juggler.addTween(indicatorBgBitmap, 1.6, Transition.linear);
-      indicatorFgSlide = stage.juggler.addTween(indicatorFgBitmap, 1.6, Transition.linear);
-      indicatorBgSlide.animate.x.to(indicatorBgBitmap.x + 374);
-      indicatorFgSlide.animate.x.to(indicatorFgBitmap.x + 374);
-      indicatorBgSlide.onComplete = () => indicatorBgSlide = null;
-      indicatorFgSlide.onComplete = () => indicatorFgSlide = null;
-
-      if (e.key == 'v') {
-        indicatorBgSlide = stage.juggler.addTween(indicatorBgBitmap, 0.001, Transition.linear);
-        indicatorFgSlide = stage.juggler.addTween(indicatorBgBitmap, 0.001, Transition.linear);
-        indicatorBgSlide.animate.x.to(indicatorBgBitmap.x);
-        indicatorFgSlide.animate.x.to(indicatorFgBitmap.x);
-
-        //indicatorBgBitmap.(Color.White);
-      }
+    if (e.key == 'v') {
+      //Start moving the indicator
+      moveIndicatorBackground = stage.juggler.addTween(indicatorBackground, 1.6, Transition.linear);
+      moveIndicatorBackground.animate.x.to(indicatorBackground.x + 384);
+      moveIndicatorForeground = stage.juggler.addTween(indicatorForeground, 1.6, Transition.linear);
+      moveIndicatorForeground.animate.x.to(indicatorForeground.x + 384);
+      //moveIndicatorBackground.onComplete = () => moveIndicatorBackground = null;
+      //moveIndicatorForeground.onComplete = () => moveIndicatorForeground = null;
+      indicator();
+      looping = true;
     }
   });
 
-    /*
-  Tween indicatorBgSlide, indicatorFgSlide;
-
-    if (indicatorBgSlide != null && indicatorFgSlide != null) return;
-
-    indicatorBgSlide = stage.juggler.addTween(indicatorBgBitmap, 1.6, Transition.linear);
-    indicatorFgSlide = stage.juggler.addTween(indicatorFgBitmap, 1.6, Transition.linear);
-
-    indicatorBgSlide.animate.x.to(indicatorBgBitmap.x + 50);
-    indicatorFgSlide.animate.x.to(indicatorFgBitmap.x + 50);
-
-    indicatorBgSlide.onComplete = () => indicatorBgSlide = null;
-    indicatorFgSlide.onComplete = () => indicatorFgSlide = null;
-    */
-
-  }
-
-  Sprite sliderIndicator() {
-    indicatorBg.graphics.rectangle(100,100,12,42);
-    indicatorBg.graphics.fillColor(Color.Black);
-    indicatorFg.grapics.rectangle(indicatorBg.x,indicatorBg.y,4,38);
-    indicatorFg.graphics.fillColor(Color.White);
-  }
-
-  Sprite generateTarget(int xpos, int ypos, int width, int height) {
-    target.graphics.rectangle(xpos,ypos,width,height);
-    target.graphics.fillColor(Color.Green);
-
-  }
-
-  void randomTargetLocation() {
-    
-  }
-
-
 }
 
-/*
-class SliderTarget extends DisplayObjectContainer {
-  
-SliderTarget() {
-    var background = new BitmapData(400, 300, false, Color.White);
-    var backgroundBitmap = new Bitmap(background);
-    addChild(backgroundBitmap);
+  void generateIndicator() {
+  indicatorBackground = Bitmap(BitmapData(8, 42, Color.Black));
+  indicatorBackground.pivotX = indicatorBackground.width / 2;
+  indicatorBackground.pivotY = indicatorBackground.height / 2;
+  indicatorBackground.x = sliderBackground.x - 192;
+  indicatorBackground.y = sliderBackground.y;
+  addChild(indicatorBackground);
 
-    for(var i = 0; i < colors.length; i++) {
-      var box = new BitmapData(100, 100, false, colors[i]);
-      var boxBitmap = new Bitmap(box);
-      boxBitmap.x = 80 + i * 50;
-      boxBitmap.y = 60 + i * 30;
-      addChild(boxBitmap);
+  indicatorForeground = Bitmap(BitmapData(4, 38, Color.White));
+  indicatorForeground.pivotX = indicatorForeground.width / 2;
+  indicatorForeground.pivotY = indicatorForeground.height / 2;
+  indicatorForeground.x = indicatorBackground.x;
+  indicatorForeground.y = indicatorBackground.y;
+  addChild(indicatorForeground);
+  }
+
+  void generateTarget() {
+
+    //TODO target moves in higher difficulty, different colors to represent different patterns
+
+    Random rand = new Random();
+
+    //random number between 10 and 30
+    int randomWidth = 10 + rand.nextInt(20);
+    //random number between +80 or -80 from the x pos center of slider background
+    int randomX = sliderBackground.x - 80 + rand.nextInt(160);
+
+    target = Bitmap(BitmapData(randomWidth, 38, Color.Green));
+    target.pivotX = target.width / 2;
+    target.pivotY = target.height / 2;
+    target.x = randomX;
+    target.y = sliderBackground.y;
+    addChild(target);
+    //TODO make target always under indicator, or indicator always above target
+  }
+
+  void indicator() {
+
+    if (looping == false) return;
+
+    //Stop moving the indicator
+    stage.juggler.clear();
+    moveIndicatorBackground = null;
+    moveIndicatorForeground = null;
+        
+    //Delay and visual effect
+
+    //Delete current target and create a new one
+    removeChild(target);
+    generateTarget();
+
+    //Hit detection
+    bool indicatorOnTarget = indicatorBackground.hitTestObject(target);
+
+    if (indicatorOnTarget) {
+      //Target hit
+
+    } else {
+      //Target missed
+
     }
+
+    //Delete current indicator and create a new one
+    removeChild(indicatorBackground);
+    removeChild(indicatorForeground);
+    generateIndicator();
+
+    moveIndicatorBackground = stage.juggler.addTween(indicatorBackground, 1.6, Transition.linear);
+    moveIndicatorBackground.animate.x.to(indicatorBackground.x + 384);
+    moveIndicatorForeground = stage.juggler.addTween(indicatorForeground, 1.6, Transition.linear);
+    moveIndicatorForeground.animate.x.to(indicatorForeground.x + 384);
   }
+
 }
-*/
--- web/main.dart
+++ web/main.dart
@@ -13,35 +13,26 @@ Future<Null> main() async {
 
   var canvas = html.querySelector('#stage');
   var stage = Stage(canvas, width: 800, height: 800, options: options);
-  //var stage = new Stage(canvas);
+  stage.pixelRatio = 1;
+  stage.scaleMode = StageScaleMode.NO_SCALE;
 
   var renderLoop = RenderLoop();
   renderLoop.addStage(stage);
 
-  var player = new Player();
-  player.x = 100;
-  player.y = 100;
+  var player = Player();
+  player.x = stage.width / 2;
+  player.y = 200;
   stage.addChild(player);
 
-  var slider = new Slider();
-  slider.x = 40;
-  slider.y = 40;
+  var slider = Slider();
+  slider.x = stage.width / 2;
+  slider.y = 400;
   stage.addChild(slider);
 
 /*
   var resourceManager = ResourceManager();
   resourceManager.addBitmapData("player0", "images/player0@1x.png");
   resourceManager.addBitmapData("player1", "images/player1@1x.png");
-  resourceManager.addBitmapData("sliderBackground", "images/sliderBackground@1x.png");
-  resourceManager.addBitmapData("slider0", "images/slider0@1x.png");
-  resourceManager.addBitmapData("slider1", "images/slider1@1x.png");
-  resourceManager.addBitmapData("reel0", "images/reel0@1x.png");
-  resourceManager.addBitmapData("reel1", "images/reel1@1x.png");
-  resourceManager.addBitmapData("cast0", "images/cast0@1x.png");
-  resourceManager.addBitmapData("cast1", "images/cast1@1x.png");
-  resourceManager.addBitmapData("sliderTarget0", "images/sliderTarget0@1x.png");
-  resourceManager.addBitmapData("sliderTarget1", "images/sliderTarget1@1x.png");
-  resourceManager.addBitmapData("sliderTarget2", "images/sliderTarget2@1x.png");
 
   await resourceManager.load();
 
@@ -49,99 +40,16 @@ Future<Null> main() async {
   var player = Sprite();
   player.addChild(Bitmap(playerData));
 
-  var sliderBackgroundData = resourceManager.getBitmapData("sliderBackground");
-  var sliderBackground = Sprite();
-  sliderBackground.addChild(Bitmap(sliderBackgroundData));
-
-  var sliderData = resourceManager.getBitmapData("slider0");
-  var slider = Sprite();
-  slider.addChild(Bitmap(sliderData));
-
-  var actionData = resourceManager.getBitmapData("cast0");
-  var action = Sprite();
-  action.addChild(Bitmap(actionData));
-
-  var sliderTargetData = resourceManager.getBitmapData("sliderTarget0");
-  var sliderTarget = Sprite();
-  sliderTarget.addChild(Bitmap(sliderTargetData));
-
   player.pivotX = playerData.width / 2;
   player.pivotY = playerData.height / 2;
-  sliderBackground.pivotX = sliderBackground.width / 2;
-  sliderBackground.pivotY = sliderBackground.height / 2;
-  slider.pivotX = slider.width / 2;
-  slider.pivotY = slider.height / 2;
-  action.pivotX = action.width / 2;
-  action.pivotY = action.height / 2;
-  sliderTarget.pivotX = sliderTarget.width / 2;
-  sliderTarget.pivotY = sliderTarget.height / 2;
-
   player.scaleX = 1;
   player.scaleY = 1;
-  sliderBackground.scaleX = 1;
-  sliderBackground.scaleY = 1;
-  slider.scaleX = 1;
-  slider.scaleY = 1;
-  action.scaleX = 1;
-  action.scaleY = 1;
-  sliderTarget.scaleX = 1;
-  sliderTarget.scaleY = 1;
 
   player.x = 100;
   player.y = 100;
-  sliderBackground.x = 400;
-  sliderBackground.y = 500;
-  action.x = 400;
-  action.y = 620;
 
   stage.addChild(player);
-  stage.addChild(sliderBackground);
-  stage.addChild(action);
 
-  Tween sliderToRight;
-
-  action.onMouseClick.listen((MouseEvent e) {
-
-    if (sliderToRight != null) return;
-
-    sliderTarget.x = 400;
-    sliderTarget.y = 500;
-    stage.addChild(sliderTarget);
-
-    slider.x = sliderBackground.x - 144 + Random.nextInt(80);
-    slider.y = 500;
-    stage.addChild(slider);
-
-    sliderToRight = stage.juggler.addTween(slider, 1.6, Transition.linear);
-    sliderToRight.animate.x.to(slider.x+369);
-    sliderToRight.onComplete = () => sliderToRight = null;
-  });
-  action.mouseCursor = MouseCursor.POINTER;
-
-  */
-
-/*
-    update(e) {
-    if (keyboard.isPressed(KeyCode.W)) {
-      var playerUp =
-      stage.juggler.addTween(player, 0.1, Transition.linear);
-      playerUp.animate.y.to(player.y-5);
-    } else if (keyboard.isPressed(KeyCode.S)) {
-      var playerDown =
-      stage.juggler.addTween(player, 0.1, Transition.linear);
-      playerDown.animate.y.to(player.y+5);
-    } else if (keyboard.isPressed(KeyCode.A)) {
-      var playerLeft =
-      stage.juggler.addTween(player, 0.1, Transition.linear);
-      playerLeft.animate.y.to(player.x-5);
-    } else if (keyboard.isPressed(KeyCode.D)) {
-      var playerRight =
-      stage.juggler.addTween(player, 0.1, Transition.linear);
-      playerRight.animate.y.to(player.x+5);
-    } else {
-
-      }
-    }
     */
 
 }
